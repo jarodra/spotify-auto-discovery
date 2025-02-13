78cab3ceff3119c13605e3b2aebe6dd3a87af819
Javier Rodrigo Alonso <javier@radevops.com>
2025-02-13 12:52:47 +0100

    Separate all functions with different method blocks

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
+import time
+from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import os
-from dotenv import load_dotenv
-import time
 from spotipy_anon import SpotifyAnon
 
 # Function to write to GitHub Action summary
@@ -20,60 +20,72 @@ def write_to_summary(message) :
         print("GITHUB_STEP_SUMMARY is not set. Printing instead.")
         print(message)
     
-# Read environment variables
-load_dotenv()
-USER_ID = os.getenv("SPOTIFY_USER")
-DISCOVER_WEEKLY_ID = os.getenv("DISCOVER_WEEKLY_ID")
-
-write_to_summary(f"## Spotify weekly save")
-
 # Authorization
-try: 
-    scope = "playlist-modify-public"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, open_browser=False))
-    sp_anon = spotipy.Spotify(auth_manager=SpotifyAnon())
-    message = "Auth completed"
-    print(message)
-    write_to_summary(f"**Step success:** {message}")
-except:
-    message = "Error during OAuth authorization"
-    write_to_summary(f"**Step failed:** {message}")
-    os._exit(1)
+def authenticate_spotify():
+    global sp, sp_anon
+    try: 
+        scope = "playlist-modify-public"
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, open_browser=False))
+        sp_anon = spotipy.Spotify(auth_manager=SpotifyAnon())
+        message = "Auth completed"
+        print(message)
+        write_to_summary(f"**Step success:** {message}")
+    except:
+        message = "Error during OAuth authorization"
+        write_to_summary(f"**Step failed:** {message}")
+        os._exit(1)
     
 # Read user playlist
-try:
-    items = sp_anon.playlist_items(DISCOVER_WEEKLY_ID)["items"]
-    tracks = [item["track"]["external_urls"]["spotify"] for item in items]
-    message = "Spotify weekly read"
-    print(message)
-    write_to_summary(f"**Step success:** {message}")
-except:
-    message = "Error when reading the playlist"
-    print(message)
-    write_to_summary(f"**Step failed:** {message}")
-    os._exit(1)
+def read_playlist():
+    global tracks
+    try:
+        items = sp_anon.playlist_items(DISCOVER_WEEKLY_ID)["items"]
+        tracks = [item["track"]["external_urls"]["spotify"] for item in items]
+        message = "Spotify weekly read"
+        print(message)
+        write_to_summary(f"**Step success:** {message}")
+    except:
+        message = "Error when reading the playlist"
+        print(message)
+        write_to_summary(f"**Step failed:** {message}")
+        os._exit(1)
     
 # Create empty playlist
-try:
-    new_playlist_name = time.strftime("discover-weekly-%Y-KW%U")
-    new_playlist = sp.user_playlist_create(USER_ID, new_playlist_name)
-    message = f"New playlist created {new_playlist_name}"
-    print(message)
-    write_to_summary(f"**Step success:** {message}")
-except:
-    message = "Error when creating the playlist"
-    print(message)
+def create_playlist():
+    global new_playlist
+    try:
+        new_playlist_name = time.strftime("discover-weekly-%Y-KW%U")
+        new_playlist = sp.user_playlist_create(USER_ID, new_playlist_name)
+        message = f"New playlist created {new_playlist_name}"
+        print(message)
+        write_to_summary(f"**Step success:** {message}")
+    except:
+        message = "Error when creating the playlist"
+        print(message)
+        write_to_summary(f"**Step failed:** {message}")
+        os._exit(1)
+
+# Add tracks to the new playlist
+def add_tracks_to_playlist():
+    try:
+        sp.playlist_add_items(new_playlist["id"], tracks, position=None)
+        message = "Songs added to the new playlist"
+        print(message)
+        write_to_summary(f"**Step success:** {message}")
+    except:
+        message = "Error when adding the songs to the playlist"
+        print(message)
     write_to_summary(f"**Step failed:** {message}")
     os._exit(1)
 
-# Add tracks to the new playlist
-try:
-    sp.playlist_add_items(new_playlist["id"], tracks, position=None)
-    message = "Songs added to the new playlist"
-    print(message)
-    write_to_summary(f"**Step success:** {message}")
-except:
-    message = "Error when adding the songs to the playlist"
-    print(message)
-    write_to_summary(f"**Step failed:** {message}")
-    os._exit(1)
+if __name__ == "__main__":
+    # Read environment variables
+    load_dotenv()
+    USER_ID = os.getenv("SPOTIFY_USER")
+    DISCOVER_WEEKLY_ID = os.getenv("DISCOVER_WEEKLY_ID")
+
+    write_to_summary(f"## Spotify weekly save")
+    authenticate_spotify()
+    read_playlist()
+    create_playlist()
+    add_tracks_to_playlist()
